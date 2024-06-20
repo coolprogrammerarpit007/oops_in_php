24d4930c548e59f6dea9eb7d803e28c570960bd5
arpit mishra <arpit.mishra.out@gmail.com>
2024-06-20 12:39:00 +0530

    Inheritance in PHP

---
+++ inheritance.php
@@ -0,0 +1,39 @@
+<?php
+
+// Inheritance in PHP
+
+class Animal{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function eating()
+    {
+        return "{$this->name} is eating food.";
+    }
+}
+
+class Dog extends Animal{
+    public function bark()
+    {
+        return "{$this->name} the Dog is barking.";
+    }
+}
+
+class Cat extends Animal{
+    public function meow()
+    {
+        return "{$this->name} the kitten is meowing!";
+    }
+}
+
+$dog1= new Dog("Johnny");
+$cat1= new Cat("Romida");
+echo $dog1->bark() . "<br>";
+echo $cat1->meow() . "<br>";
+echo $cat1->eating() . "<br>";
+
+?>
