5f434a0390a38dcd0f8f42e4290efd82ae655c8e
arpit mishra <arpit.mishra.out@gmail.com>
2024-06-21 14:57:43 +0530

    Mobile Sim Class problem

---
+++ pratice_problems/mobile.php
@@ -0,0 +1,64 @@
+<?php
+
+    // Mobile Class
+
+    abstract class SIM{
+
+        abstract public function calling();
+        abstract public function message();
+
+    }
+
+    class BSNL extends SIM{
+        private $company;
+        private $year;
+
+        public function __construct($company,$year)
+        {
+         $this->company = $company;
+         $this->year = $year;   
+        }
+
+        public function calling(){
+            return "Calling with the {$this->company} sim.";
+        }
+
+
+        public function message(){
+            return "Messaging with the {$this->company} sim.";
+        }
+        
+    };
+    class AIRTEL extends SIM{
+        private $company;
+        private $year;
+
+        public function __construct($company,$year)
+        {
+         $this->company = $company;
+         $this->year = $year;   
+        }
+
+        public function calling(){
+            return "Calling with the {$this->company} sim." ;
+        }
+
+
+        public function message(){
+            return "Messaging with the {$this->company} sim.";
+        }
+        
+    };
+
+
+    $mobileUser1 = new BSNL("Bsnl",1997);
+    $mobileUser2 = new AIRTEL("Airtel",2003);
+
+
+    // echo $mobileUser1->calling();
+    // echo $mobileUser1->message();
+    // echo $mobileUser2->calling();
+    // echo $mobileUser2->message();
+
+    
+
